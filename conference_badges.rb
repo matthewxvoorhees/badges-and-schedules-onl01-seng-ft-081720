fa0fa21c43779c8ef7c92be51c8e9ce8810321ef
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-19 19:08:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,6 @@
-badge_maker(speakers)
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+badge_maker(speakers)
 speakers.each do |badge_name|
   puts "Hello, my name is #{badge_name}."
 end
